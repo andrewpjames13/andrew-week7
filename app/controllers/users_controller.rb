354a3d1384a6fa32748ef1a3cfdd665fbb4cc196
Andrew James <andrewpjames87@gmail.com>
2015-02-23 10:06:48 -0700

    User can create a new user and view on user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,28 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :about)
+  end
 end
